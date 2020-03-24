a56f065b5bf9a11711a7d4c03d1b2c8c9630260b
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-03-24 01:22:48 -0300

    now using environments

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -2,45 +2,23 @@ import { Injectable } from '@angular/core';
 import { Blog } from '../models/Blog';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  // blogs: Blog[] = [
-  //   {
-  //     title: 'Toma control sobre tus limpiezas',
-  //     // tslint:disable-next-line: max-line-length
-  // tslint:disable-next-line: max-line-length
-  //     content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam eos nesciunt, reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum?`,
-  //     imgUrl: 'libros.jpg'
-  //   },
-  //   {
-  //     title: 'lorem',
-  //     // tslint:disable-next-line: max-line-length
-  // tslint:disable-next-line: max-line-length
-  //     content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam eos nesciunt, reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum?`,
-  //     imgUrl: 'bosque.jpg'
-  //   },
-  //   {
-  //     title: 'blog3',
-  //     // tslint:disable-next-line: max-line-length
-  // tslint:disable-next-line: max-line-length
-  //     content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae vel necessitatibus cumque beatae! Eligendi unde quam eos nesciunt, reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum reprehenderit dicta dignissimos, odio quasi, debitis asperiores libero aperiam maiores doloremque rerum?`,
-  //     imgUrl: 'estudio.jpg'
-  //   },
-  // ];
+  baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   getBlogs(): Observable<Blog[]> {
-    return this.http.get<Blog[]>('https://steffani-mendoza-api.herokuapp.com/blogs');
+    return this.http.get<Blog[]>(this.baseUrl + 'blogs/');
   }
 
   getBlog(id: string): Observable<Blog> {
-    return this.http.get<Blog>(`https://steffani-mendoza-api.herokuapp.com/blogs/${id}`);
+    return this.http.get<Blog>(this.baseUrl + 'blogs/' + id);
   }
-
 
 }
