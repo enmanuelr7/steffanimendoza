a2d74ea34f1f58d19fe53e026976c6f2eed75f02
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-04-03 22:47:21 -0300

    version 1.1

--- src/app/components/content/category/quotes.ts
+++ src/app/components/content/category/quotes.ts
@@ -2,26 +2,26 @@ export const quotes = [
     {
         name: 'beauty',
         // tslint:disable-next-line: max-line-length
-        quote: '"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, itaque doloremque. Quam quo ea eum rerum, dolore quod quisquam adipisci."',
-        author: 'beauty'
+        quote: '"La belleza de las cosas existe en el espíritu del que las contempla."',
+        author: 'David Hume'
     },
     {
         name: 'fitness',
         // tslint:disable-next-line: max-line-length
-        quote: '"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, itaque doloremque. Quam quo ea eum rerum, dolore quod quisquam adipisci."',
-        author: 'fitness'
+        quote: '"No dejamos de hacer ejercicio porque envejecemos, envejecemos porque dejamos de hacer ejercicio."',
+        author: 'Dr. Kenneth Cooper'
     },
     {
         name: 'food',
         // tslint:disable-next-line: max-line-length
-        quote: '"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, itaque doloremque. Quam quo ea eum rerum, dolore quod quisquam adipisci."',
-        author: 'food'
+        quote: '"Cada célula viviente en tu cuerpo está hecha de la comida que comes. Si constantemente comes comida chatarra entonces tendrás un cuerpo chatarra."',
+        author: 'Jeanette Jenkins'
     },
     {
         name: 'mindfulness',
         // tslint:disable-next-line: max-line-length
-        quote: '"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, itaque doloremque. Quam quo ea eum rerum, dolore quod quisquam adipisci."',
-        author: 'mindfulness'
+        quote: '"El secreto de la salud mental y física no es llorar por el pasado, preocuparse por el futuro o anticipar problemas, sino vivir en el momento presente sabiamente."',
+        author: 'Buda'
     }
 ];
 
